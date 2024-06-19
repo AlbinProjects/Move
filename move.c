2e239c3a7c4180526ff7fe0b80435abc32ef1c3d
AlbinProjects <albinjoy1230@gmail.com>
2024-06-19 16:17:51 +0530

    move.c

--- move.c
+++ move.c
@@ -4,30 +4,28 @@
 #include <string.h>
 #include <errno.h>
 
-//
+// move error function 
 void print_error(char *filename1, char *filename2){
     fprintf(stderr, "Output error: cannot move %s to %s\n%s\n", 
              filename1, filename2, strerror(errno));
     exit(EXIT_FAILURE);
 }
 
-//
+// invalid format error function 
 void print_usage(){
     fprintf(stderr, "SYNTAX ERROR:\nUsage: [old filename] [new filename]\n");
     exit(EXIT_FAILURE);
 }
-
-
-
            
            
 int main(int argc, char *argv[])
 {
 
 #ifdef _WIN32
-	//check for windows
+	// check for windows
         printf("The program is not supported on Windows, please try in Linux\n");
         return 0;
+	
     // Check for Linux
     #elif defined(__linux__)
         printf("The program is running on Linux.\n");
@@ -45,15 +43,14 @@ int main(int argc, char *argv[])
 		print_usage(argv[0]);
 	}
 	
+	int status = rename(argv[1], argv[2]); 
 	
-	int status = rename(argv[1], argv[2]);  //
-	
-	
+	// move error
     if(status == -1)
     {
         print_error(argv[1], argv[2]);
     }
-    else if(status == 0)
+    else if(status == 0) // success
     {
     fprintf(stderr, "Operation Success..");
     }
